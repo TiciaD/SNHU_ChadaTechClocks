e59a5445c4828158b2fc6558cbc9bc6c9b5fc3e2
Ticia D <84279128+TiciaD@users.noreply.github.com>
2022-03-18 23:52:39 -0400

    started project added first function

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+// Monticia Dunn
+// CS 210 Project One
+#include <iostream>
+using namespace std;
+#include <string>
+#include <ctime>
+#include <time.h>
+#include <conio.h>
+
+
+// takes in hour, minute or second and increments it
+int incrementTime(int currUnit) {
+	currUnit = currUnit + 1;
+	return currUnit;
+}
